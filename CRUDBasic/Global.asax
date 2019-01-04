ed6cdfe87a235ad1ec226c61db40f4199695f857
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2019-01-04 18:46:50 -0300

    CRUD BASICO

---
+++ CRUDBasic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRUDBasic.MvcApplication" Language="C#" %>
---
+++ CRUDBasic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUDBasic.Startup))]
+namespace CRUDBasic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
